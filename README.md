f869f7dce8ef8d73bab0a93fcf71e2ee5303dc05
pecurka <ana4991@gmail.com>
2016-10-30 22:29:09 +0100

    Add dog model

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ The main goal is to create a Frogger replica with the playable character being a
 Added main file for project. <br />
 Added MakeFile. <br />
 Make initial window for project. <br />
+
+# 30.10.2016 Update Notes:
+Change .gitignore for project. <br />
+Made dog model using 3D objects. <br />
+![alt tag](https://github.com/MATF-RG16/RG16-006-plugger/img/screenshot_30_10.png)
--- src/main.c
+++ src/main.c
@@ -7,8 +7,11 @@ static void on_reshape(int width, int height);
 static void on_display(void);
 
 
-int main(int argc, char **argv)
-{
+/* Metod koji crta model psa */
+static void draw_dog(void);
+
+
+int main(int argc, char **argv) {
     /* Inicijalizuje se GLUT. */
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
@@ -34,8 +37,7 @@ int main(int argc, char **argv)
 }
 
 
-static void on_reshape(int width, int height)
-{
+static void on_reshape(int width, int height) {
     /* Postavlja se viewport. */
     glViewport(0, 0, width, height);
 
@@ -45,16 +47,152 @@ static void on_reshape(int width, int height)
     gluPerspective(45, (float) width / height, 1, 1500);
 }
 
-static void on_display(void)
-{
+static void on_display(void) {
     /* Postavlja se boja svih piksela na zadatu boju pozadine. */
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     /* Postavlja se vidna tacka. */
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    gluLookAt(2, 5, 10, 0, 0, 0, 0, 1, 0);
+    gluLookAt(5, 5, 10, 0, 0, 0, 0, 1, 0);
 
+
+    /* Postavljamo sliku psa */
+    glPushMatrix();
+
+
+    draw_dog();
+
+    glPopMatrix();
     /* Postavlja se nova slika u prozor. */
     glutSwapBuffers();
+}
+
+static void draw_dog(void) {
+#define glutCube glutSolidCube
+
+  /* Postavljamo boju tela psa */
+  glColor3f(.75, .6, .4);
+
+  /* Crtamo telo */
+  glPushMatrix();
+    glScalef(2, 1, 1);
+    glutCube(1);
+  glPopMatrix();
+
+  /* Crtamo rep */
+  glPushMatrix();
+    glColor3f(.3, .2, .1);
+    glTranslatef(-1.5, .4, 0);
+    glScalef(1, .2, .2);
+    glutCube(1);
+  glPopMatrix();
+
+  /* Crtamo noge */
+  glPushMatrix();
+    glColor3f(.75, .6, .4);
+    glTranslatef(-.7, -1, -.3);
+    glScalef(.3, 1, .3);
+    glutCube(1);
+  glPopMatrix();
+
+  glPushMatrix();
+    glTranslatef(-.7, -1, .3);
+    glScalef(.3, 1, .3);
+    glutCube(1);
+  glPopMatrix();
+
+  glPushMatrix();
+    glTranslatef(.7, -1, -.3);
+    glScalef(.3, 1, .3);
+    glutCube(1);
+  glPopMatrix();
+
+  glPushMatrix();
+    glTranslatef(.7, -1, .3);
+    glScalef(.3, 1, .3);
+    glutCube(1);
+  glPopMatrix();
+
+  /* Crtamo glavu */
+  glPushMatrix();
+    glTranslatef(1, .8, 0);
+    glScalef(.7, .7, 1);
+    glutCube(1);
+  glPopMatrix();
+
+  /* Crtamo njusku */
+  glPushMatrix();
+    glColor3f(.3, .2, .1);
+    glTranslatef(1.5, .6, 0);
+    glScalef(.4, .4, .4);
+    glutCube(1);
+  glPopMatrix();
+
+  /* Crtamo usi */
+  glPushMatrix();
+    glColor3f(.3, .2, .1);
+    glTranslatef(1, 1.2, -.3);
+    glScalef(.1, .1, .1);
+    glBegin( GL_TRIANGLES );
+      glVertex3f( 0.0f, 1.f, 0.0f );
+      glVertex3f( -1.0f, -1.0f, 1.0f );
+      glVertex3f( 1.0f, -1.0f, 1.0f);
+
+      glVertex3f( 0.0f, 1.0f, 0.0f);
+      glVertex3f( -1.0f, -1.0f, 1.0f);
+      glVertex3f( 0.0f, -1.0f, -1.0f);
+
+      glVertex3f( 0.0f, 1.0f, 0.0f);
+      glVertex3f( 0.0f, -1.0f, -1.0f);
+      glVertex3f( 1.0f, -1.0f, 1.0f);
+
+
+      glVertex3f( -1.0f, -1.0f, 1.0f);
+      glVertex3f( 0.0f, -1.0f, -1.0f);
+      glVertex3f( 1.0f, -1.0f, 1.0f);
+    glEnd();
+  glPopMatrix();
+
+  glPushMatrix();
+    glColor3f(.3, .2, .1);
+    glTranslatef(1, 1.2, .3);
+    glScalef(.1, .1, .1);
+    glBegin( GL_TRIANGLES );
+      glVertex3f( 0.0f, 1.f, 0.0f );
+      glVertex3f( -1.0f, -1.0f, 1.0f );
+      glVertex3f( 1.0f, -1.0f, 1.0f);
+
+      glVertex3f( 0.0f, 1.0f, 0.0f);
+      glVertex3f( -1.0f, -1.0f, 1.0f);
+      glVertex3f( 0.0f, -1.0f, -1.0f);
+
+      glVertex3f( 0.0f, 1.0f, 0.0f);
+      glVertex3f( 0.0f, -1.0f, -1.0f);
+      glVertex3f( 1.0f, -1.0f, 1.0f);
+
+
+      glVertex3f( -1.0f, -1.0f, 1.0f);
+      glVertex3f( 0.0f, -1.0f, -1.0f);
+      glVertex3f( 1.0f, -1.0f, 1.0f);
+    glEnd();
+  glPopMatrix();
+
+  /* Crtamo oci */
+  glPushMatrix();
+    glColor3f(0, 0, 0);
+    glTranslatef(1.35, .9, -.3);
+    glScalef(.1, .1, .1);
+    glutSolidSphere(1, 20, 20);
+  glPopMatrix();
+
+  glPushMatrix();
+    glColor3f(0, 0, 0);
+    glTranslatef(1.35, .9, .3);
+    glScalef(.1, .1, .1);
+    glutSolidSphere(1, 20, 20);
+  glPopMatrix();
+
+
+#undef glutCube
 }
