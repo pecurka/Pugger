166805193b329cf32260030757d24a1a42e5a188
pecurka <ana4991@gmail.com>
2016-12-18 23:55:09 +0100

    Refactor drawing functions into seperate file

---
+++ src/objects.c
@@ -0,0 +1,173 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <GL/glut.h>
+#include "objects.h"
+
+void draw_dog(void) {
+#define glutCube glutSolidCube
+
+  /* Postavljamo boju tela psa */
+  glColor3f(.75, .6, .4);
+
+  /* Crtamo telo */
+  glPushMatrix();
+    glScalef(2, 1, 1);
+    glutCube(1);
+  glPopMatrix();
+
+  /* Crtamo rep */
+  glPushMatrix();
+    glColor3f(.3, .2, .1);
+    glTranslatef(-1.5, .4, 0);
+    glScalef(1, .2, .2);
+    glutCube(1);
+  glPopMatrix();
+
+  /* Crtamo noge */
+  glPushMatrix();
+    glColor3f(.75, .6, .4);
+    glTranslatef(-.7, -1, -.3);
+    glScalef(.3, 1, .3);
+    glutCube(1);
+  glPopMatrix();
+
+  glPushMatrix();
+    glTranslatef(-.7, -1, .3);
+    glScalef(.3, 1, .3);
+    glutCube(1);
+  glPopMatrix();
+
+  glPushMatrix();
+    glTranslatef(.7, -1, -.3);
+    glScalef(.3, 1, .3);
+    glutCube(1);
+  glPopMatrix();
+
+  glPushMatrix();
+    glTranslatef(.7, -1, .3);
+    glScalef(.3, 1, .3);
+    glutCube(1);
+  glPopMatrix();
+
+  /* Crtamo glavu */
+  glPushMatrix();
+    glTranslatef(1, .8, 0);
+    glScalef(.7, .7, 1);
+    glutCube(1);
+  glPopMatrix();
+
+  /* Crtamo njusku */
+  glPushMatrix();
+    glColor3f(.3, .2, .1);
+    glTranslatef(1.5, .6, 0);
+    glScalef(.4, .4, .4);
+    glutCube(1);
+  glPopMatrix();
+
+  /* Crtamo usi */
+  glPushMatrix();
+    glColor3f(.3, .2, .1);
+    glTranslatef(1, 1.2, -.3);
+    glScalef(.1, .1, .1);
+    glBegin( GL_TRIANGLES );
+      glVertex3f( 0.0f, 1.f, 0.0f );
+      glVertex3f( -1.0f, -1.0f, 1.0f );
+      glVertex3f( 1.0f, -1.0f, 1.0f);
+
+      glVertex3f( 0.0f, 1.0f, 0.0f);
+      glVertex3f( -1.0f, -1.0f, 1.0f);
+      glVertex3f( 0.0f, -1.0f, -1.0f);
+
+      glVertex3f( 0.0f, 1.0f, 0.0f);
+      glVertex3f( 0.0f, -1.0f, -1.0f);
+      glVertex3f( 1.0f, -1.0f, 1.0f);
+
+
+      glVertex3f( -1.0f, -1.0f, 1.0f);
+      glVertex3f( 0.0f, -1.0f, -1.0f);
+      glVertex3f( 1.0f, -1.0f, 1.0f);
+    glEnd();
+  glPopMatrix();
+
+  glPushMatrix();
+    glColor3f(.3, .2, .1);
+    glTranslatef(1, 1.2, .3);
+    glScalef(.1, .1, .1);
+    glBegin( GL_TRIANGLES );
+      glVertex3f( 0.0f, 1.f, 0.0f );
+      glVertex3f( -1.0f, -1.0f, 1.0f );
+      glVertex3f( 1.0f, -1.0f, 1.0f);
+
+      glVertex3f( 0.0f, 1.0f, 0.0f);
+      glVertex3f( -1.0f, -1.0f, 1.0f);
+      glVertex3f( 0.0f, -1.0f, -1.0f);
+
+      glVertex3f( 0.0f, 1.0f, 0.0f);
+      glVertex3f( 0.0f, -1.0f, -1.0f);
+      glVertex3f( 1.0f, -1.0f, 1.0f);
+
+
+      glVertex3f( -1.0f, -1.0f, 1.0f);
+      glVertex3f( 0.0f, -1.0f, -1.0f);
+      glVertex3f( 1.0f, -1.0f, 1.0f);
+    glEnd();
+  glPopMatrix();
+
+  /* Crtamo oci */
+  glPushMatrix();
+    glColor3f(0, 0, 0);
+    glTranslatef(1.35, .9, -.3);
+    glScalef(.1, .1, .1);
+    glutSolidSphere(1, 20, 20);
+  glPopMatrix();
+
+  glPushMatrix();
+    glColor3f(0, 0, 0);
+    glTranslatef(1.35, .9, .3);
+    glScalef(.1, .1, .1);
+    glutSolidSphere(1, 20, 20);
+  glPopMatrix();
+
+
+#undef glutCube
+}
+
+void draw_floor(GLuint grassTexture) {
+
+  /* Trava */
+  /* Gasi se osvetljenje i pali se tekstura trave */
+  glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+  glBindTexture(GL_TEXTURE_2D, grassTexture);
+
+  glPushMatrix();
+    glBegin(GL_QUADS);
+      glNormal3f(0, 1, 0);
+      glTexCoord2f(0, 0); glVertex3f(-20,0,-30);
+      glTexCoord2f(10, 0); glVertex3f(-12,0,-30);
+      glTexCoord2f(10, 10); glVertex3f(-12,0,30);
+      glTexCoord2f(0, 10); glVertex3f(-20,0,30);
+    glEnd();
+
+    glBegin(GL_QUADS);
+      glNormal3f(0, 1, 0);
+      glTexCoord2f(0, 0); glVertex3f(20,0,-30);
+      glTexCoord2f(10, 0); glVertex3f(40,0,-30);
+      glTexCoord2f(10, 10);glVertex3f(40,0,30);
+      glTexCoord2f(0, 10); glVertex3f(20,0,30);
+    glEnd();
+
+    /* Gasi se tekstura i ponovo se pokrece osvetljenje*/
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+
+    /* Autoput */
+    glColor3f(0.2,0.2,0.2);
+    glBegin(GL_QUADS);
+      glNormal3f(0, 1, 0);
+      glVertex3f(-12,0,-30);
+      glVertex3f(20,0,-30);
+      glVertex3f(20,0,30);
+      glVertex3f(-12,0,30);
+    glEnd();
+  glPopMatrix();
+}
---
+++ src/objects.h
@@ -0,0 +1,11 @@
+#ifndef _H_OBJECTS_H_
+#define _H_OBJECTS_H_
+
+
+/* Metod koji crta model  psa */
+void draw_dog(void);
+
+/* Metod koji crta model poda */
+void draw_floor(GLuint grassTexture);
+
+#endif
